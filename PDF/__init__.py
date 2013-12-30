81c6ccdd2a2055b48c986f494feba3d880c69b34
Jason R. Coombs <jaraco@jaraco.com>
2013-12-29 20:43:52 -0500

    Correct __all__
    --HG--
    extra : histedit_source : 69f68e1634992f66e696310367dfe2a778ef2521

--- PDF/__init__.py
+++ PDF/__init__.py
@@ -3,6 +3,6 @@ import pkg_resources
 from .pdf import PdfFileReader, PdfFileWriter
 from .merger import PdfFileMerger
 
-__all__ = ["pdf", "PdfFileMerger"]
+__all__ = ["PdfFileMerger", "PdfFileReader", "PdfFileWriter"]
 
 __version__ = pkg_resources.require('PDF')[0].version
